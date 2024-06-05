43483a0e996b3722c8e53b1f77b7bf2afe9d6487
José_DaniloS <154340363+JoseDaniloS@users.noreply.github.com>
2024-06-05 00:29:34 -0300

    MODIFICAÇÃO NA LUZ script.js
    CORRIGINDO O EFEITO DA LUZ NO .JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         function ligar(){
             let luz = window.document.getElementById('luz')
             
-            luz.style.filter = "opacity(0.3) drop-shadow(0 7px 50px #ffff)"
+            luz.style.filter = "opacity(0.1) drop-shadow(0 7px 10px #ffff)"
             
         }
 
